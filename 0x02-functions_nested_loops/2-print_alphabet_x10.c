03ebb4e62f1be26434a2927418b9e56a71c1e1a2
Herbalcode <adeboyahmad@gmail.com>
2022-09-14 21:46:10 +0100

    doing c at basic

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *Print_alphabet - to print abc x10
+ *
+ *Return:0
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			{
+			_putchar ('\n')
+		}
+}
