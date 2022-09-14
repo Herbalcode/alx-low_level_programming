e7174c4857d259d16062e33878b7a895690679f7
Herbalcode <adeboyahmad@gmail.com>
2022-09-14 13:33:43 +0100

    function to print a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * print_alphabet - function to
+ *
+ * print a-z alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (b='a';b<='z';b++)
+	{
+		_putchar (c);
+
+	}
+
+_putchar('\n');
+
+}
