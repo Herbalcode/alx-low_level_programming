862c19a87505996a9f785626951916189c3f07e0
Herbalcode <adeboyahmad@gmail.com>
2022-09-15 12:31:52 +0100

    addition of number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - adds two integers
+ *
+ *function that adds two number and return value
+ *
+ * Return: the result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+	
