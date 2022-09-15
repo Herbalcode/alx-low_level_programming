0563c10b2f55806971bae96ced0823f047e5891a
Herbalcode <adeboyahmad@gmail.com>
2022-09-15 04:17:11 +0100

    value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,4 @@
-#imclude "main.h"
+#include "main.h"
 #include <stdio.h>
 /**
  *function that computes the absolute value numb
@@ -9,7 +9,7 @@ int _abs(int c)
 {
 	if (c > 0 || c == 0)
 	{
-	return (c);
+	return (c);	
 	}
 	else
 	return (c * -1);
