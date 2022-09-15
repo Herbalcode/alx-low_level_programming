28867ec73074ca3097d9a2146a6344b2365d3de6
Herbalcode <adeboyahmad@gmail.com>
2022-09-15 18:32:49 +0100

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,24 +2,24 @@
 
 /**
  *
- *main - cause an infitie loop
+ *main - to cause an infitie loop
  *Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 
-	printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-	return (0);
+return (0);
 }
