68a11c8fa771cd88346d0c0ef776a0a69e0bc55c
Herbalcode <adeboyahmad@gmail.com>
2022-09-16 05:59:22 +0100

    nested project

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers 
+ *
+ * Return:
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
