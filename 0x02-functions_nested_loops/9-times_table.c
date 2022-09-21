6ec90aecffe94a007d29389aa8edd8b20dcda4e0
Herbalcode <adeboyahmad@gmail.com>
2022-09-21 19:16:11 +0100

    learning looping

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,42 @@
 #include "main.h"
+
 /**
- *_abs - prints the 9 times table
- *
- *function that prints the 9 times table 
- *
+ * times_table - prints the 9 times table
+ * Description: Above
  * Return: void
  */
+
 void times_table(void)
 {
-	int num, mult, prod;
+	int row, column, product, tens, ones;
 
-	for (num = 0; num <= 9; num++)
+	for (row = 0; row <= 9; row++)
 	{
-		_putchar('0');
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
 
-	for (mult = 1; mult <= 9; mult++)
-	{
-		_putchar('.');
-		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
-		_putchar(' ');
-		else
-		_putchar((prod / 10) + '0');
-		_putchar((prod % 10) + '0');
-	}
-	_putchar('\n');
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
 	}
 }
