ef0f66ca15c63cfb7a068893fb9e80915140fe76
Herbalcode <adeboyahmad@gmail.com>
2022-10-03 07:47:06 +0100

    alx task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * program that print it name
@@ -7,7 +8,7 @@
  * Return: 0 successful
  */
 
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
         (void) argc;
         printf("%s\n", argv[0]);
