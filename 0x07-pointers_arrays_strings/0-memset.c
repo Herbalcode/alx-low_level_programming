9e17f1c9e36313b6fb1d98079494843221e77a70
Herbalcode <adeboyahmad@gmail.com>
2022-10-08 16:11:08 +0100

    alx task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,46 +1,24 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * memset fuction- fill the memory with a constant bytes
+ * fills the first n bytes of the memory area poin
+ * to by s with the constant b
+ * @s: pointer to a memory location
+ * @b: constant to fill memory
+ * @n: bytes of the memory area to be filled
  *
- * Return: Nothing.
+ * Return: pointer to memmory s
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+char *_memset(char *s, char b, unsigned int n);
+{   
+       unsigned int counter;
+       for (counter = 0; counter < n; counter++);
+       {
+	       s[counter] = b;
+       }
+		return (s);
+       
 }
