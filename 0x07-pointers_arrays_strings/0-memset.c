98f7d14f6c396b00d4896a96213a48f4daedab29
Herbalcode <adeboyahmad@gmail.com>
2022-10-10 04:32:52 +0100

    alx task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * memset fuction- fill the memory with a constant bytes
- * fills the first n bytes of the memory area poin
- * to by s with the constant b
- * @s: pointer to a memory location
- * @b: constant to fill memory
- * @n: bytes of the memory area to be filled
+ *_memset - fills memory with a constant byte.
+ *fills the first n bytes of the memory area pointed
+ *to by s with the constant b.
+ *@s: pointer to memory area.
+ *@b: constant to fill memory with.
+ *@n: bytes of the memory area to be filled.
  *
- * Return: pointer to memmory s
- */
+ *Return: pointer to memory area s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int counter;
 
-char *_memset(char *s, char b, unsigned int n);
-{   
-       unsigned int counter;
-
-       for (counter = 0; counter < n; counter++);
-       {
-	       s[counter] = b;
-       }
-		return (s);
-       
+	for (counter = 0; counter < n; counter++)
+	{
+		s[counter] = b;
+	}
+	return (s);
 }
