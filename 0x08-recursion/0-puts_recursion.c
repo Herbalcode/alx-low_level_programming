fd61cad49ef16c0fcd9660f1fd73abbd04d4a8c5
Herbalcode <adeboyahmad@gmail.com>
2022-11-20 01:58:37 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+void _puts_recursion(char *s);
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Fuctions in these codes act in recursive manner.
